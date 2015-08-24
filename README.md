499a73b5404ab9942a490331d4487637804e7293
fraserxu <xvfeng123@gmail.com>
2015-08-24 20:30:28 +0800

    add electron-prepuilt as a dependency

--- README.md
+++ README.md
@@ -20,18 +20,6 @@ npm install electron-pdf -g
 Usage
 -----
 
-### Install Electron
-
-First, you need to install Electron. You can either run:
-
-```
-npm install -g electron-prebuilt
-```
-
-and then `electron` will be added to your path. Or, you can download a version from https://github.com/atom/electron/releases and then set an environment variable ELECTRON_PATH pointing to the binary. Note if you're using Mac OS X, the path would be to the actual executable and not the app directory e.g. `/Applications/Electron.app/Contents/MacOS/Electron.`
-
-You should probably just install `electron-prebuilt` as it simplifies things.
-
 ### Build PDF
 
 ```
--- cli.js
+++ cli.js
@@ -2,33 +2,17 @@
 
 var path = require('path')
 var spawn = require('child_process').spawn
-var which = require('which')
+var electronPath = require('electron-prebuilt')
 
-which('electron', function (err, resolvedPath) {
-  if (!resolvedPath && !process.env.ELECTRON_PATH) {
-    console.error('')
-    console.error('  Can not find `electron` in the $PATH and $ELECTRON_PATH is not set.')
-    console.error('  Please either set $ELECTRON_PATH or run `npm i -g electron-prebuilt`.')
-    console.error('')
-    process.exit(1)
-  }
-
-  if (!resolvedPath) return runElectron(process.env.ELECTRON_PATH)
-  if (resolvedPath) return runElectron(resolvedPath)
-  if (err) console.error(err)
+var args = process.argv.slice(2)
+args.unshift(path.resolve(path.join(__dirname, './index.js')))
+var electron = spawn(electronPath, args, {
+  stdio: ['inherit', 'inherit', 'pipe', 'ipc'],
+  cwd: path.resolve(__dirname)
 })
-
-function runElectron (resolvedPath) {
-  var args = process.argv.slice(2)
-  args.unshift(path.resolve(path.join(__dirname, './index.js')))
-  var electron = spawn(resolvedPath, args, {
-    stdio: ['inherit', 'inherit', 'pipe', 'ipc'],
-    cwd: path.resolve(__dirname)
-  })
-  electron.stderr.on('data', function (data) {
-    var str = data.toString('utf8')
-    // it's Chromium, STFU
-    if (str.match(/^\[\d+\:\d+/)) return
-    process.stderr.write(data)
-  })
-}
+electron.stderr.on('data', function (data) {
+  var str = data.toString('utf8')
+  // it's Chromium, STFU
+  if (str.match(/^\[\d+\:\d+/)) return
+  process.stderr.write(data)
+})
