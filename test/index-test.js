b562f3e254b05a1ae52d0db553e6797b8ad6449f
Nate Good <codecounselor@gmail.com>
2016-10-13 17:54:59 -0400

    Jasmine to ava (#114)
    Switched to AVA, except for tests that require an electron process

---
+++ test/index-test.js
@@ -0,0 +1,22 @@
+import {test} from 'ava'
+
+// var path = require('path')
+// var spawn = require('child_process').spawn
+// var electronPath = require('electron')
+
+import ElectronPDF from '../lib/index'
+
+// var indexPath = path.resolve(path.join(__dirname, '../lib/charge.js'))
+// var electron = spawn(electronPath, [indexPath])
+//
+// test.after.always('guaranteed cleanup', t => {
+//    electron.kill()
+// });
+
+test.skip('Initialization triggers ready event', t => {
+  const pdf = new ElectronPDF()
+  pdf.on('charged', () => {
+    t.pass()
+  })
+  pdf.start()
+})
