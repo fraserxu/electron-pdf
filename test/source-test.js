a32ccba93d306a318b33606fa128aca37fb26062
Nate Good <codecounselor@gmail.com>
2016-11-10 10:54:07 -0500

    Fixes #129 (#130) - Enhanced filename suffixes and fixed markdown conversion
    * Need to parse pageSize dimensions in contructor
    
    * don't add file index to name as suffix for single inputs, add index before extension for multiple inputs, fixes #129
    
    * added bluebird dependency for Promises

--- test/source-test.js
+++ test/source-test.js
@@ -4,12 +4,20 @@ var Source = require('../lib/source')
 var source = new Source()
 
 test('resolve() handles a string', t => {
-  let result = source.resolve('http://www.google.com')
-  t.is(result[0], 'http://www.google.com')
+  source.resolve('http://www.google.com').then(result => {
+    t.is(result[0], 'http://www.google.com')
+  })
 })
 
 test('resolve() handles arrays of strings', t => {
-  let result = source.resolve(['http://www.google.com', 'local.html'])
-  t.is(result.length, 2)
+  source.resolve(['http://www.google.com', 'local.html']).then(result => {
+    t.is(result.length, 2)
+  })
 })
 
+test('resolve() converts markdown to html', t => {
+  source.resolve(['../README.md'], {}).then(result => {
+    t.is(result.length, 1)
+    t.truthy(result[0].endsWith('.html'))
+  })
+})
