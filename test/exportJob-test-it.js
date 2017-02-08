d05d0b9d5818bf58b5033661a059a539c3d7c114
codecounselor <nate.good@spiderstrategies.com>
2017-02-08 11:57:18 -0500

    better naming and documentation #161

--- test/exportJob-test-it.js
+++ test/exportJob-test-it.js
@@ -15,7 +15,7 @@ test('_readyEventTimeout removes ipc listener', t => {
   const generateFunction = () => {}
   const ipcListener = job._attachIPCListener('myEvent', generateFunction)
   t.is(electron.ipcMain.listenerCount('READY_TO_RENDER'), 1)
-  job._cancelReadyEventTimeout('myEvent', ipcListener, generateFunction)
+  job._cancelReadyEvent('myEvent', ipcListener, generateFunction)
   t.is(electron.ipcMain.listenerCount('READY_TO_RENDER'), 0)
 })
 
