4f076bb17183541e7fc8400b8df0f5da034e5e5a
Nate Good <codecounselor@gmail.com>
2017-02-17 12:59:55 -0500

    merging 1.2 into master, 1.1 branch for patches only (#178)
    * exportJob has become massive, so window size operations are now in windowTailor #172
    * support orientations #172
    * logger injection #172
    * Reverted back to did-finish-load listeners, because newer implementation broke ready events #173

---
+++ test/logger-test.js
@@ -0,0 +1,22 @@
+import { test } from 'ava'
+
+import _ from 'lodash'
+
+import { set, info } from '../lib/logger'
+
+/**
+ * Shows usage for clients, and validates tha it works!
+ */
+test('set loggers', t => {
+  let msg
+  // Can set only certain loggers, and let others use debug logging
+  const loggers = {
+    info: function () { msg = 'id ' + _.join(arguments, ' ') }
+  }
+  set(loggers)
+  // all varargs should just get passed through and logged however the
+  // provided logger chooses to log them
+  info('then', 'args')
+
+  t.is(msg, 'id then args')
+})
