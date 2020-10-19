0602972edfeb29ae345e2303eadb93ac26340165
Nate Good <nate.good@spiderstrategies.com>
2020-10-19 12:00:43 -0400

    listen for window close event, code refactoring for #273

--- test/exportJob-test-it.js
+++ test/exportJob-test-it.js
@@ -15,6 +15,6 @@ test('_readyEventTimeout removes ipc listener', t => {
   const generateFunction = () => {}
   const ipcListener = job._attachIPCListener('myEvent', generateFunction)
   t.is(electron.ipcMain.listenerCount('READY_TO_RENDER'), 1)
-  job._cancelReadyEvent('myEvent', ipcListener, generateFunction)
+  job._proceedWithExport('myEvent', ipcListener, generateFunction)
   t.is(electron.ipcMain.listenerCount('READY_TO_RENDER'), 0)
 })
